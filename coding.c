7aeed589d09fdc9caa525612fccb8f3e4f5baace
Martin Hundebøll <github@hunn.dk>
2011-03-09 11:03:08 +0100

    Created coding files

---
+++ coding.c
@@ -0,0 +1,7 @@
+#include "coding.h"
+
+void add_orig_neighbor(struct ethhdr *ethhdr,
+        struct batman_packet *batman_packet)
+{
+
+}
---
+++ coding.h
@@ -0,0 +1,6 @@
+#ifndef _NET_BATMAN_ADV_CODING_H
+#define _NET_BATMAN_ADV_CODING_H
+
+void add_orig_neighbor(struct ethhdr *ethhdr, struct batman_packet *batman_packet);
+
+#endif
